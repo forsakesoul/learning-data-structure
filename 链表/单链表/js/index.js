6332e38c0cc0c6ff73ded329907f0f065f225ba6
forsakesoul <lfc060725@163.com>
2019-06-24 23:52:07 +0800

    list-1

--- 链表/单链表/js/index.js
+++ 链表/单链表/js/index.js
@@ -7,6 +7,52 @@ class Node {
 
 class Link {
   constructor() {
-    
+    this.head = new Node('head')
+  }
+
+  // 根据值查找
+  findByValue(item) {
+    let currentNode = this.head.next
+    while (currentNode !== null && currentNode.element !== item) {
+      currentNode = currentNode.next
+    }
+    return currentNode === null ? -1 : currentNode
+  }
+
+  // 根据节点查找
+  findByIndex(index) {
+    let currentNode = this.head.next
+    let pos = 0
+    while (currentNode !== null && pos !== index) {
+      currentNode = currentNode.next
+      pos++
+    }
+    return currentNode === null ? -1 : pos
+  }
+
+  append(newEle) {
+    const newNode = new Node(newEle)
+    let currentNode = this.head
+    while (currentNode.next) {
+      currentNode = currentNode.next
+    }
+    currentNode.next = newNode
+  }
+
+  insert(newElement, element) {
+    const currentNode = this.findByValue(element)
+    if (currentNode === -1) {
+      console.log('没找到');
+      return;
+    }
+    const newNode = new Node(newElement)
+    newNode.next = currentNode.next
+    currentNode.next = newNode
   }
 }
+
+const list = new Link()
+console.log('list', list)
+list.append('one')
+list.append('two')
+console.log('list', list)
